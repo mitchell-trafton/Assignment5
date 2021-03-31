12be811d3a6bab0474f2e4025fe4c3f35544db1f
Mitchell <mastrafton@gmail.com>
2021-03-31 17:48:21 -0500

    Added puzzle form

--- Assignment5/PuzzleForm.Designer.cs
+++ Assignment5/PuzzleForm.Designer.cs
@@ -48,7 +48,7 @@ namespace Assignment5
             this.textBox1.Location = new System.Drawing.Point(3, 3);
             this.textBox1.MaxLength = 1;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(26, 30);
+            this.textBox1.Size = new System.Drawing.Size(74, 30);
             this.textBox1.TabIndex = 0;
             // 
             // PuzzleForm
